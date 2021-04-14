805421d4229acb2fa1a22f35ef49910fe896b362
ald-ac <muriasaldair@hotmail.com>
2021-04-13 23:09:06 -0500

    Creando arreglo con direcciones de imgs y objetos con propiedades para integrarlas en el HTML

--- app.js
+++ app.js
@@ -2,16 +2,40 @@
 const tarjetas = document.getElementsByClassName('rounded');
 const arrayTarjetas = [...tarjetas];
 
+let objImagenes = [];
+
 //Listeners
 listeners();
 
 function listeners() {
+
+    document.addEventListener('DOMContentLoaded', cargarArregloImg);
+
     arrayTarjetas.forEach(tarjeta => {
         tarjeta.addEventListener('click', voltearTarjeta);
     });
 }
 
 //Funciones
+function cargarArregloImg() {
+    imagenes.forEach(imagen => {
+        
+        let objImagen = {
+            dir: '',
+            estado: 0,
+            acierto: 0
+        }
+
+        objImagen.dir = imagen;
+        objImagenes.push(objImagen);
+    });
+
+    arrayTarjetas.forEach(tarj => {
+        tarj.src = objImagenes[0].dir;
+    })
+
+}
+
 function voltearTarjeta() {
-    console.log("Todo bien");
+    
 }
---
+++ imagenes.js
@@ -0,0 +1,8 @@
+let imagenes = [
+    './assets/img1.png',
+    './assets/img2.png',
+    './assets/img3.png',
+    './assets/img4.png',
+    './assets/img5.png',
+    './assets/img6.png'
+];
